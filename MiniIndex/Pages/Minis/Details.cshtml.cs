2369ef5aa8560d09a14f5acfddaff36ac54fe59b
Andy Luhrs <aluhrs@microsoft.com>
2019-12-19 21:59:34 -0800

    Adding super basic tag suggestions based on mini name, only visible to moderators.

--- MiniIndex/Pages/Minis/Details.cshtml.cs
+++ MiniIndex/Pages/Minis/Details.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using MiniIndex.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Threading.Tasks;
 
 namespace MiniIndex.Pages.Minis
@@ -14,6 +15,8 @@ namespace MiniIndex.Pages.Minis
         public Mini Mini { get; set; }
         public List<Tag> UnusedTags { get; set; }
         public List<Tag> MiscTags { get; set; }
+        public List<Tag> WordMatchingTags { get; set; }
+        public List<Tag> SimilarTags { get; set; }
 
         public DetailsModel(MiniIndexContext context)
         {
@@ -34,6 +37,9 @@ namespace MiniIndex.Pages.Minis
                 .Include(m => m.User)
                 .FirstOrDefaultAsync(m => m.ID == id);
 
+
+
+
             UnusedTags = _context
                 .Tag
                 .AsEnumerable()
@@ -41,6 +47,35 @@ namespace MiniIndex.Pages.Minis
                 .OrderBy(m => m.Category.ToString())
                 .ThenBy(m => m.TagName)
                 .ToList();
+
+
+            if (User.IsInRole("Moderator"))
+            {
+                string[] NameSplit = Mini.Name.ToLower().Split(' ');
+
+                WordMatchingTags = _context.Tag
+                    .Where(t => NameSplit.Contains(t.TagName.ToLower()))
+                    .ToList();
+
+                //TODO - I'm sure someone who knows LINQ could clean this up a lot.
+                if (WordMatchingTags.Count > 0)
+                {
+                    List<Mini> SimilarMinis = _context.Mini
+                                .Include(m => m.MiniTags)
+                                    .ThenInclude(mt => mt.Tag)
+                                    .AsEnumerable()
+                                .Where(m => m.MiniTags.Select(mt => mt.Tag).Intersect(WordMatchingTags).Any())
+                                .ToList();
+
+                    List<MiniTag> SimilarMiniTags = SimilarMinis.SelectMany(m => m.MiniTags).ToList();
+                    SimilarTags = SimilarMiniTags.GroupBy(mt => mt.Tag).Select(grp => grp.First().Tag).Except(Mini.MiniTags.Select(mt => mt.Tag)).ToList();
+                }
+                else
+                {
+                    SimilarTags = new List<Tag>();
+                }
+            }
+
 
             if (Mini == null)
             {
