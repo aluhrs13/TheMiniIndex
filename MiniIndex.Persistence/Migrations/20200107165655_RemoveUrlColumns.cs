ccca120439fc120ebb17cb4a62e48ab388bc816f
anaximander <nickbudd@gmail.com>
2020-01-07 17:02:14 +0000

    Fixing migrations to avoid deployment errors

---
+++ MiniIndex.Persistence/Migrations/20200107165655_RemoveUrlColumns.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MiniIndex.Migrations
+{
+    public partial class RemoveUrlColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PatreonURL",
+                table: "Creator");
+
+            migrationBuilder.DropColumn(
+                name: "ShapewaysURL",
+                table: "Creator");
+
+            migrationBuilder.DropColumn(
+                name: "ThingiverseURL",
+                table: "Creator");
+
+            migrationBuilder.DropColumn(
+                name: "WebsiteURL",
+                table: "Creator");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PatreonURL",
+                table: "Creator",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShapewaysURL",
+                table: "Creator",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ThingiverseURL",
+                table: "Creator",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WebsiteURL",
+                table: "Creator",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
