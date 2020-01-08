9460e2e38febf39a8468e942c49fad97f47ed431
Andy Luhrs <aluhrs@microsoft.com>
2020-01-08 09:33:57 -0800

    Two minor changes to possibly improve preprod.

--- MiniIndex/Program.cs
+++ MiniIndex/Program.cs
@@ -22,13 +22,6 @@ namespace MiniIndex
                 .UseLamar()
                 .ConfigureAppConfiguration((context, config) =>
                 {
-                    Assembly assembly = typeof(Program).Assembly;
-                    string basePath = assembly.Location.Replace(assembly.ManifestModule.Name, String.Empty, StringComparison.Ordinal);
-
-                    config
-                        .SetBasePath(basePath)
-                        .AddJsonFile("localsettings.json", optional: true);
-
                     if (context.HostingEnvironment.IsProduction())
                     {
                         IConfigurationRoot builtConfig = config.Build();
