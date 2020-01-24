f8fba5147d4f282a350a14795edcec374cabd97e
anaximander <nickbudd@gmail.com>
2020-01-24 11:38:00 +0000

    npm and webpack seem to be working.

---
+++ MiniIndex/webpack.config.js
@@ -0,0 +1,32 @@
+const glob = require('glob');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: {
+        site: './wwwroot/js/site.js',
+        bootstrap_js: './wwwroot/js/bootstrapjs.js',
+        validation: './wwwroot/js/validation.js',
+        tags: './wwwroot/js/tags-selectized.js',
+        stars: './wwwroot/js/stars.js'
+    },
+    output: {
+        filename: '[name].entry.js',
+        path: __dirname + '/wwwroot/dist'
+    },
+    devtool: 'source-map',
+    mode: 'development',
+    module: {
+        rules: [
+            { test: /\.css$/, use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader"] },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
+            { test: /\.(woff|woff2)$/, loader: "url-loader?prefix=font/&limit=5000" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        })
+    ]
+};
---
+++ MiniIndex/wwwroot/js/tags-selectized.js
@@ -0,0 +1,96 @@
+﻿$('.add-tag').click(function () {
+    document.getElementById("AddedTags").innerHTML = document.getElementById("AddedTags").innerHTML.concat("<span class='badge badge-success'>" + this.innerHTML.substr(2, this.innerHTML.length) + "</span>");
+    console.log("Adding " + this.innerHTML);
+    $(this).fadeOut();
+    $.get({
+        url: "/MiniTags/Create/",
+        data: { mini: document.getElementById("miniid").innerHTML, tag: this.id },
+        complete: function () {
+        },
+    });
+
+    $('#tagSearch').val('');
+    return false;
+});
+
+$('.remove-tag').click(function () {
+    //document.getElementById("UnusedTags").innerHTML = document.getElementById("UnusedTags").innerHTML.concat("<span class='badge badge-success'>" + this.innerHTML.substr(2, this.innerHTML.length) + "</span>");
+
+    console.log("Removing " + this.innerHTML);
+    $.get({
+        url: "/MiniTags/Delete/",
+        data: { mini: document.getElementById("miniid").innerHTML, tag: this.id },
+        complete: function () {
+        },
+    });
+    $(this).hide();
+    return false;
+});
+
+$('#tagSearch').on('input', function (e) {
+    $('.add-tag').hide();
+    $('.add-tag-div').hide();
+    var tagFilter = $(this).val().trim().toLowerCase();
+
+    $('.add-tag').each(function () {
+        if ($(this).text().toLocaleLowerCase().indexOf(tagFilter) >= 0) {
+            $(this).show();
+            $(this).parent().show();
+        }
+    });
+
+    return false;
+});
+
+$('#AddNewTag').click(function () {
+    var newTag = $("#tagSearch").val();
+    document.getElementById("UsedTags").innerHTML = document.getElementById("UsedTags").innerHTML.concat("<span class='badge badge-success'>" + newTag + "</span>");
+    $.get({
+        url: "/MiniTags/Create/",
+        data: { mini: document.getElementById("miniid").innerHTML, tagName: newTag },
+        complete: function () {
+        },
+    });
+    return false;
+});
+
+$('.change-category').change(function () {
+    $.get({
+        url: "/Tags/Edit/",
+        data: { id: this.parentElement.parentElement.id, category: $(this).children("option:selected").text() },
+        complete: function () {
+        },
+    });
+
+    return false;
+});
+
+$('#selectizedinput').selectize({
+    valueField: 'tagName',
+    labelField: 'tagName',
+    searchField: 'tagName',
+    plugins: ['remove_button'],
+    delimiter: ',',
+    persist: false,
+    openOnFocus: false,
+    closeAfterSelect: true,
+    options: [],
+    create: false,
+    render: {
+        option: function (item, escape) {
+            return '<div>' +
+                '<span class="title">' +
+                '<span class="name">' + escape(item.tagName) + '</span>' +
+                '</span>' +
+                '</div>';
+        }
+    }
+});
+
+$('.add-to-filters').click(function () {
+    var $select = $('#selectizedinput').selectize();
+    var selectize = $select[0].selectize;
+
+    selectize.addItem($(this).text());
+    return false;
+});
