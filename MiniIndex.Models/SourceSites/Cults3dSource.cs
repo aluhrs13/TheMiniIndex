d3ad677b8bcf8ff1bad3abc6ac5203ac01f2e528
Andy Luhrs <aluhrs@microsoft.com>
2020-01-30 19:36:53 -0800

    Adding Cults3d support

---
+++ MiniIndex.Models/SourceSites/Cults3dSource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MiniIndex.Models.SourceSites
+{
+    public class Cults3dSource : SourceSite
+    {
+        public Cults3dSource(Creator creator, string cults3dUsername)
+            : base(creator)
+        {
+            CreatorUserName = cults3dUsername;
+        }
+
+        protected Cults3dSource()
+        {
+        }
+
+        public override Uri BaseUri => new Uri("https://cults3d.com/en/users/");
+        public override Uri CreatorPageUri => new Uri(BaseUri, CreatorUserName);
+    }
+}
