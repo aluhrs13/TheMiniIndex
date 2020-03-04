967f5abad34be9a92e0ec16565c16c14912e9540
Andrew Luhrs <aluhrs@outlook.com>
2020-03-04 15:12:56 -0800

    Moving to saas Bootstrap to enable more configuration.

--- MiniIndex/wwwroot/js/site.js
+++ MiniIndex/wwwroot/js/site.js
@@ -1,3 +1,2 @@
-﻿import 'bootstrap/dist/css/bootstrap.css'
-import 'open-iconic/font/css/open-iconic-bootstrap.min.css'
+﻿import 'open-iconic/font/css/open-iconic-bootstrap.min.css'
 import '../css/site.css'
