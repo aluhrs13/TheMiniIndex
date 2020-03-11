d2bc39ee0a2db16d5a94da75bd9bdb85ca2507ca
Andy Luhrs <aluhrs@outlook.com>
2020-03-11 11:41:56 -0700

    Moving thumbnails to Azure Blob (#170)
    * Uploading of the full image to blob and setting it to be the main image.
    
    * Resizing of images before uploading to blob
    
    * Smarter resizing and handle local builds without blob key set.

---
+++ MiniIndex.Core/Utilities/AzureStorageConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniIndex.Core.Utilities
+{
+    public class AzureStorageConfig
+    {
+        public string AccountName { get; set; }
+        public string AccountKey { get; set; }
+        public string ImageContainer { get; set; }
+        public string ThumbnailContainer { get; set; }
+    }
+}
