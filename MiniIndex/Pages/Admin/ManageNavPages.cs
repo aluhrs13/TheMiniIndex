a6f543e16e8dc18d44997dbe641a4c5ffa7d3232
Andrew Luhrs <aluhrs@outlook.com>
2020-03-19 08:30:02 -0700

    Adding a tag stream page for moderators (more useful after #186)

--- MiniIndex/Pages/Admin/ManageNavPages.cs
+++ MiniIndex/Pages/Admin/ManageNavPages.cs
@@ -12,11 +12,14 @@ namespace MiniIndex.Pages.Admin
         public static string Tags => "Tags";
         public static string Users => "Users";
         public static string Creators => "Creators";
+        public static string TagStream => "TagStream";
+
 
         public static string MinisNavClass(ViewContext viewContext) => PageNavClass(viewContext, Minis);
         public static string TagsNavClass(ViewContext viewContext) => PageNavClass(viewContext, Tags);
         public static string UsersNavClass(ViewContext viewContext) => PageNavClass(viewContext, Users);
         public static string CreatorsNavClass(ViewContext viewContext) => PageNavClass(viewContext, Creators);
+        public static string TagStreamNavClass(ViewContext viewContext) => PageNavClass(viewContext, TagStream);
 
 
         private static string PageNavClass(ViewContext viewContext, string page)
---
+++ MiniIndex/Pages/Admin/TagStream.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MiniIndex.Models;
+using MiniIndex.Persistence;
+
+namespace MiniIndex.Pages.TagStream
+{
+    [Authorize]
+    public class TagStreamModel : PageModel
+    {
+        private readonly MiniIndexContext _context;
+        public IList<MiniTag> UserTags { get; set; }
+
+        public TagStreamModel(MiniIndexContext context)
+        {
+            _context = context;
+        }
+
+        public async Task OnGetAsync()
+        {
+            UserTags = _context.MiniTag
+                .Include(mt => mt.Tagger)
+                .Include(mt=>mt.Mini)
+                .Include(mt=>mt.Tag)
+                .Where(mt=>mt.Tagger!=null)
+                .ToList();
+
+            UserTags = UserTags.Take(100).ToList();
+        }
+    }
+}
