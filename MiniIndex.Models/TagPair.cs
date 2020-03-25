3845ec791c0d60436f417cf84660b2d2a3f0fdc7
Andrew Luhrs <aluhrs@outlook.com>
2020-03-25 15:23:46 -0700

    Adding times to MiniTag and TagPair concept

---
+++ MiniIndex.Models/TagPair.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniIndex.Models
+{
+    public enum PairType
+    {
+        Synonym,
+        Parent //For Parent/Child relationships, Tag1 is the child and Tag2 is the parent.
+    }
+
+    public class TagPair
+    {
+        public TagPair()
+        {
+        }
+
+        public int ID { get; set; }
+        public Tag Tag1 { get; set; }
+        public Tag Tag2 { get; set; }
+        public PairType Type { get; set; }
+
+        public Tag GetPairedTag(Tag seedTag)
+        {
+            if(seedTag.ID == Tag1.ID)
+            {
+                return Tag2;
+            }
+            else
+            {
+                return Tag1;
+            }
+        }
+    }
+}
