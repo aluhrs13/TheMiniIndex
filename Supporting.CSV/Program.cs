f768e480ddb5cbcf3fbe73a9b2879ac7c3f253b3
Andrew Luhrs <aluhrs@outlook.com>
2020-04-22 08:47:23 -0700

    Cleaning up CSV parser a bit

--- Supporting.CSV/Program.cs
+++ Supporting.CSV/Program.cs
@@ -12,64 +12,76 @@ namespace MiniIndex.CSV
 
         static async System.Threading.Tasks.Task Main(string[] args)
         {
+            bool addThingiverse = false;
             Dictionary<string, int> DomainCount = new Dictionary<string, int>();
             int i = 0;
 
-            HtmlWeb web = new HtmlWeb();
-            HtmlDocument htmlDoc = web.Load("C:\\Users\\aluhr\\Downloads\\PC NPC Classes.html");
-            //HtmlDocument htmlDoc = web.Load("C:\\Users\\aluhr\\Downloads\\Bestiary.html");
-            HtmlNodeCollection links = htmlDoc.DocumentNode.SelectNodes("//a");
 
-            foreach (HtmlNode link in links)
+            if (args.Length == 1)
             {
-                Uri linkURL = new Uri(link.GetAttributeValue("href", String.Empty));
-
-                if (!DomainCount.ContainsKey(linkURL.Host))
+                if(args[0] == "thingiverse")
                 {
-                    DomainCount.Add(linkURL.Host, 1);
+                    addThingiverse = true;
                 }
-                else
+            }
+
+            HtmlWeb web = new HtmlWeb();
+
+            string folder = "C:\\Users\\aluhr\\Downloads\\MMBest\\";
+            string[] sheets = new string[]{ "Bestiary.html", "PC NPC Classes.html", "Uncategorized (Plz Send Help).html" };
+
+
+            foreach(string sheet in sheets)
+            {
+                HtmlDocument htmlDoc = web.Load(folder + sheet);
+                HtmlNodeCollection links = htmlDoc.DocumentNode.SelectNodes("//a");
+
+                foreach (HtmlNode link in links)
                 {
-                    DomainCount[linkURL.Host]++;
-                }
+                    Uri linkURL = new Uri(link.GetAttributeValue("href", String.Empty));
 
-                if (!linkURL.Host.Contains("thingiverse"))
-                {
-                    continue;
-                }
-
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://www.theminiindex.com/api/minis/create?url=" + linkURL.ToString() + "&key=" + AutoCreateKey);
-                //HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://miniindex-preproduction.azurewebsites.net/api/minis/create?url=" + word + "&key=" + AutoCreateKey);
-                //HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://localhost:44386/api/minis/create?url=" + linkURL.ToString() + "&key=" + AutoCreateKey);
-                Console.WriteLine("Looking at - " + linkURL);
-
-
-
-                try
-                {
-                    using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
-                    using (Stream stream = response.GetResponseStream())
-                    using (StreamReader reader = new StreamReader(stream))
+                    if (!DomainCount.ContainsKey(linkURL.Host))
                     {
-                        Console.WriteLine("\t Status - " + response.StatusCode);
-                        Console.WriteLine("\t Mini URL - " + await reader.ReadToEndAsync());
+                        DomainCount.Add(linkURL.Host, 1);
+                    }
+                    else
+                    {
+                        DomainCount[linkURL.Host]++;
+                    }
+
+                    if (linkURL.Host.Contains("thingiverse") && addThingiverse == false)
+                    {
+                        continue;
+                    }
+
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://www.theminiindex.com/api/minis/create?url=" + linkURL.ToString() + "&key=" + AutoCreateKey);
+                    Console.WriteLine("Looking at - " + linkURL);
+
+                    try
+                    {
+                        using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+                        using (Stream stream = response.GetResponseStream())
+                        using (StreamReader reader = new StreamReader(stream))
+                        {
+                            Console.WriteLine("\t Status - " + response.StatusCode);
+                            Console.WriteLine("\t Mini URL - " + await reader.ReadToEndAsync());
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("\t Exception - " + ex.Message);
+                    }
+
+                    i++;
+                    if (i >= 50)
+                    {
+                        i = 0;
+                        Console.WriteLine("Waiting...");
+                        System.Threading.Thread.Sleep(60000);
                     }
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("\t Exception - " + ex.Message);
-                }
-
-
-                i++;
-                if (i >= 50)
-                {
-                    i = 0;
-                    Console.WriteLine("Waiting...");
-                    System.Threading.Thread.Sleep(60000);
-                }
-
             }
+
 
             foreach (KeyValuePair<string, int> Site in DomainCount)
             {
