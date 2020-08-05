a5e3b813112a8d8bd65c4aeaa4608fc67fa922f9
Andrew Luhrs <aluhrs@outlook.com>
2020-08-04 23:46:39 -0700

    Adding MMF and Thingiverse, as well as links from creator pages.

---
+++ Supporting.ProfileParsing/JSONClasses/ThingiverseObject.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Supporting.ProfileParsing.JSONClasses
+{
+    public class ThingiverseObject
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+        public string public_url { get; set; }
+        public DateTime created_at { get; set; }
+        public string thumbnail { get; set; }
+        public string preview_image { get; set; }
+        public Creator creator { get; set; }
+        public int is_private { get; set; }
+        public int is_purchased { get; set; }
+        public int is_published { get; set; }
+        public int comment_count { get; set; }
+        public int like_count { get; set; }
+        public Tag[] tags { get; set; }
+        public object is_nsfw { get; set; }
+    }
+
+    public class Creator
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string url { get; set; }
+        public string public_url { get; set; }
+        public string thumbnail { get; set; }
+        public int count_of_followers { get; set; }
+        public int count_of_following { get; set; }
+        public int count_of_designs { get; set; }
+        public bool accepts_tips { get; set; }
+        public bool is_following { get; set; }
+        public string location { get; set; }
+        public string cover { get; set; }
+    }
+
+    public class Tag
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        public int count { get; set; }
+        public string things_url { get; set; }
+        public string absolute_url { get; set; }
+    }
+
+
+}
--- Supporting.ProfileParsing/ProfileParser.cs
+++ Supporting.ProfileParsing/ProfileParser.cs
@@ -14,12 +14,15 @@ using System.Linq;
 using System.Net.Http;
 using Supporting.ProfileParsing.JSONClasses;
 using System.Net;
+using System.ComponentModel.Design.Serialization;
 
 namespace Supporting.ProfileParsing
 {
     public static class ProfileParser
     {
         public static string AutoCreateKey = System.Environment.GetEnvironmentVariable("AutoCreateKey", EnvironmentVariableTarget.Process);
+        public static string ThingiverseKey = System.Environment.GetEnvironmentVariable("ThingiverseKey", EnvironmentVariableTarget.Process);
+        public static string MyMiniFactoryKey = System.Environment.GetEnvironmentVariable("MyMiniFactoryKey", EnvironmentVariableTarget.Process);
 
         [FunctionName("ProfileParser")]
         public static async Task<IActionResult> Run(
@@ -28,11 +31,19 @@ namespace Supporting.ProfileParsing
         {
             string url = req.Query["url"];
 
-            log.LogInformation("[Main] Looking at - " + url.ToString());
-            await ParseURLAsync(log, new Uri(url));
+            string key = req.Query["key"];
 
-            string responseMessage = $"Scanned {url}";
-            return new OkObjectResult(responseMessage);
+            if(key == AutoCreateKey)
+            {
+                log.LogInformation("[Main] Looking at - " + url.ToString());
+                await ParseURLAsync(log, new Uri(url));
+                return new OkObjectResult($"Scanned {url}");
+
+            }
+            else
+            {
+                return new BadRequestObjectResult($"Invalid Key");
+            }
         }
 
         public static async Task ParseURLAsync(ILogger log, Uri url)
@@ -45,6 +56,16 @@ namespace Supporting.ProfileParsing
                 log.LogInformation("[Parsing URL] Gumroad URL, looking for links...");
                 GumroadParserAsync(log, url);
             }
+            else if (URLHost.Equals("thingiverse.com", StringComparison.OrdinalIgnoreCase))
+            {
+                log.LogInformation("[Parsing URL] Thingiverse URL, looking for links...");
+                ThingiverseParserAsync(log, url);
+            }
+            else if (URLHost.Equals("myminifactory.com", StringComparison.OrdinalIgnoreCase))
+            {
+                log.LogInformation("[Parsing URL] MyMiniFactory URL, looking for links...");
+                MyMiniFactoryParserAsync(log, url);
+            }
             else
             {
                 log.LogError("[Parsing URL] Invalid URL - " +url.ToString());
@@ -53,8 +74,6 @@ namespace Supporting.ProfileParsing
 
         public static async Task GumroadParserAsync(ILogger log, Uri url)
         {
-            List<Uri> urlList = new List<Uri>();
-
             HtmlWeb web = new HtmlWeb();
             HtmlDocument htmlDoc = await web.LoadFromWebAsync(url, null, null);
             HtmlNode creatorNode = htmlDoc.DocumentNode.SelectNodes("//div[contains(@class,'creator-profile-wrapper')]").FirstOrDefault();
@@ -78,7 +97,7 @@ namespace Supporting.ProfileParsing
 
                 if (response.StatusCode == System.Net.HttpStatusCode.OK)
                 {
-                    log.LogInformation("[Gumroad Parsing] Got JSON");
+                    log.LogInformation("[Gumroad Parsing] Got JSON for #"+from);
                     using (StreamReader reader = new StreamReader(await responseContent.ReadAsStreamAsync()))
                     {
                         string result = await reader.ReadToEndAsync();
@@ -86,6 +105,11 @@ namespace Supporting.ProfileParsing
 
                         htmlDoc.LoadHtml(rootObject.products_html);
                         HtmlNodeCollection nodeCollection = htmlDoc.DocumentNode.SelectNodes("//li[contains(@class,'product-card')]");
+
+                        if(nodeCollection == null)
+                        {
+                            break;
+                        }
 
                         foreach (HtmlNode node in nodeCollection)
                         {
@@ -105,6 +129,122 @@ namespace Supporting.ProfileParsing
             }
         }
 
+        public static async Task ThingiverseParserAsync(ILogger log, Uri url)
+        {
+            HttpClient httpClient = new HttpClient();
+            int page = 1;
+            string user = url.ToString().Split('/').Last();
+
+            while (true)
+            {
+                HttpResponseMessage response = await httpClient.GetAsync("https://api.thingiverse.com/Users/" + user + "/things?access_token=" + ThingiverseKey + "&page=" + page);
+                HttpContent responseContent = response.Content;
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    log.LogInformation("[Thingiverse Parsing] Got JSON");
+                    using (StreamReader reader = new StreamReader(await responseContent.ReadAsStreamAsync()))
+                    {
+                        string result = await reader.ReadToEndAsync();
+
+                        try
+                        {
+                            List<ThingiverseObject> rootObject = JsonConvert.DeserializeObject<List<ThingiverseObject>>(result);
+
+                            if (rootObject.Count == 0)
+                            {
+                                log.LogInformation("[Thingiverse Parsing] All out of items!");
+                                break;
+                            }
+
+                            if (rootObject == null)
+                            {
+                                log.LogError("[Thingiverse Parsing] No object found!");
+                            }
+
+                            foreach (ThingiverseObject item in rootObject)
+                            {
+                                Uri foundUrl = new Uri(item.public_url);
+
+                                log.LogInformation("[Thingiverse Parsing] Found URL - " + foundUrl.ToString());
+                                SubmitLinkAsync(log, foundUrl);
+                            }
+
+                        }
+                        catch (Exception ex)
+                        {
+                            log.LogError("[Thingiverse Parser] Exception getting thing list - " + ex.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    log.LogError("[Thingiverse Parsing] Error loading JSON - " + response.StatusCode);
+                }
+
+                TryWait(log, page);
+                page++;
+            }
+        }
+
+        public static async Task MyMiniFactoryParserAsync(ILogger log, Uri url)
+        {
+            HttpClient httpClient = new HttpClient();
+            int page = 1;
+            string user = url.ToString().Split('/').Last();
+
+            while (true)
+            {
+                HttpResponseMessage response = await httpClient.GetAsync("https://www.myminifactory.com/api/v2/users/" + user + "/objects?key=" + MyMiniFactoryKey + "&page=" + page);
+                HttpContent responseContent = response.Content;
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    log.LogInformation("[MyMiniFactory Parsing] Got JSON");
+                    using (StreamReader reader = new StreamReader(await responseContent.ReadAsStreamAsync()))
+                    {
+                        string result = await reader.ReadToEndAsync();
+
+                        try
+                        {
+                            MyMiniFactoryObject rootObject = JsonConvert.DeserializeObject<MyMiniFactoryObject>(result);
+
+                            if(rootObject.items.Length == 0)
+                            {
+                                log.LogInformation("[MyMiniFactory Parsing] All out of items!");
+                                break;
+                            }
+
+                            if (rootObject == null)
+                            {
+                                log.LogError("[MyMiniFactory Parsing] No object found!");
+                            }
+
+                            foreach (Item item in rootObject.items)
+                            {
+                                Uri foundUrl = new Uri(item.url);
+
+                                log.LogInformation("[MyMiniFactory Parsing] Found URL - " + foundUrl.ToString());
+                                SubmitLinkAsync(log, foundUrl);
+                            }
+
+                        }
+                        catch (Exception ex)
+                        {
+                            log.LogError("[MyMiniFactory Parser] Exception getting object list - " + ex.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    log.LogError("[MyMiniTfactory Parsing] Error loading JSON - " + response.StatusCode);
+                }
+
+                TryWait(log, page);
+                page++;
+            }
+        }
+
         public static async Task SubmitLinkAsync(ILogger log, Uri url)
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://www.theminiindex.com/api/minis/create?url=" + url.ToString() + "&key=" + AutoCreateKey);
@@ -116,13 +256,21 @@ namespace Supporting.ProfileParsing
                 using (Stream stream = response.GetResponseStream())
                 using (StreamReader reader = new StreamReader(stream))
                 {
-                    log.LogInformation("[Mini Submission] Status - " + response.StatusCode);
-                    log.LogInformation("[Mini Submission] Mini URL - " + await reader.ReadToEndAsync());
+                    log.LogInformation("[Mini Submission] Completed (" + response.StatusCode  + ") Mini URL - " + await reader.ReadToEndAsync());
                 }
             }
             catch (Exception ex)
             {
-                log.LogError("[Mini Submission] Exception - " + ex.Message);
+                log.LogError("[Mini Submission] Exception (" + url + ") - " + ex.Message);
+            }
+        }
+
+        public static void TryWait(ILogger log, int page)
+        {
+            if(page % 5 == 0)
+            {
+                log.LogWarning("Waiting ---------------------------------------------------------");
+                System.Threading.Thread.Sleep(5000);
             }
         }
     }
