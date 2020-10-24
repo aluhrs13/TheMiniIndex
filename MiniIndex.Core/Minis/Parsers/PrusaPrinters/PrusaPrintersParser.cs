149f7f955053ddea0c6d896c24f90d808d45bf88
Andrew Luhrs <aluhrs@outlook.com>
2020-10-23 21:27:16 -0700

    Get GraphQL working (still needs to fix creator links)

--- MiniIndex.Core/Minis/Parsers/PrusaPrinters/PrusaPrintersParser.cs
+++ MiniIndex.Core/Minis/Parsers/PrusaPrinters/PrusaPrintersParser.cs
@@ -1,6 +1,7 @@
 ﻿using HtmlAgilityPack;
 using MiniIndex.Models;
 using MiniIndex.Models.SourceSites;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -39,41 +40,23 @@ namespace MiniIndex.Core.Minis.Parsers.PrusaPrinters
             string id = url.LocalPath.Split('-').First().Split('/').Last();
 
             HttpClient client = new HttpClient();
-
-            // Add a new Request Message
+            
             HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, "https://www.prusaprinters.org/graphql/");
             requestMessage.Headers.Add("ContentType", "application/json");
 
-            string myContent = "{\"operationName\":\"PrintProfile\",\"variables\":{\"id\":\"30323\"},\"query\":\"query PrintProfile($id: ID!) {\n  print(id: $id) {\n    id\n    slug\n    name\n    user {\n      id\n      slug\n      email\n      donationLinks {\n        id\n        title\n        url\n        __typename\n      }\n      publicUsername\n      avatarFilePath\n      __typename\n    }\n    ratingAvg\n    myRating\n    ratingCount\n    content\n    category {\n      id\n      path {\n        id\n        name\n        __typename\n      }\n      __typename\n    }\n    modified\n    firstPublish\n    datePublished\n    hasModel\n    summary\n    shareCount\n    printDuration\n    numPieces\n    weight\n    nozzleDiameters\n    usedMaterial\n    layerHeights\n    materials {\n      name\n      __typename\n    }\n    dateFeatured\n    downloadCount\n    displayCount\n    pdfFilePath\n    commentCount\n    userGcodeCount\n    userGcodesCount\n    remixCount\n    canBeRated\n    inMyCollections\n    printer {\n      id\n      name\n      __typename\n    }\n    images {\n      id\n      filePath\n      __typename\n    }\n    tags {\n      name\n      id\n      __typename\n    }\n    thingiverseLink\n    filesType\n    foundInUserGcodes\n    remixParents {\n      ...remixParentDetail\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment remixParentDetail on PrintRemixType {\n  id\n  parentPrintName\n  parentPrintAuthor {\n    id\n    slug\n    publicUsername\n    __typename\n  }\n  parentPrint {\n    id\n    name\n    slug\n    datePublished\n    images {\n      id\n      filePath\n      __typename\n    }\n    license {\n      id\n      name\n      disallowRemixing\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\"}";
+            string myContent = @"{
+                ""operationName"": ""PrintProfile"",
+                ""variables"":{""id"": ""44053""},
+                ""query"":""query PrintProfile($id: ID!) { print(id: $id) { id slug name user { id slug publicUsername} images { filePath } } }""
+            }";
 
             requestMessage.Content = new StringContent(myContent, Encoding.UTF8, "application/json");
-
             HttpResponseMessage response = await client.SendAsync(requestMessage);
             string responseString = await response.Content.ReadAsStringAsync();
 
+            JObject text = JObject.Parse(responseString);
 
-            //Parse JSON
-
-            //Pray?
-
-
-            HtmlWeb web = new HtmlWeb();
-            
-            HtmlDocument htmlDoc = await web.LoadFromWebAsync(url, null, null);
-
-            string creatorName = htmlDoc.DocumentNode
-                .SelectNodes("//a[@data-sw-tracking-link-id=\"view-profile\"]")
-                .FirstOrDefault()
-                .GetAttributeValue("data-sw-tracking-target-entity-id", null);
-
-            Dictionary<string, string> miniProperties = htmlDoc.DocumentNode.SelectNodes("//div[@itemtype=\"http://schema.org/Product\"]/meta")
-                .Select(node => new
-                {
-                    property = node.GetAttributeValue("itemprop", null),
-                    content = node.GetAttributeValue("content", null)
-                })
-                .Where(node => !String.IsNullOrWhiteSpace(node.property))
-                .ToDictionary(k => k.property, v => v.content);
+            string creatorName = text["data"]["print"]["user"]["publicUsername"].ToString();
 
             Creator creator = new Creator
             {
@@ -85,9 +68,9 @@ namespace MiniIndex.Core.Minis.Parsers.PrusaPrinters
             Mini mini = new Mini()
             {
                 Creator = creator,
-                Name = System.Web.HttpUtility.HtmlDecode(miniProperties["name"]),
-                Thumbnail = miniProperties["image"],
-                Link = miniProperties["url"]
+                Name = text["data"]["print"]["name"].ToString(),
+                Thumbnail = "https://media.prusaprinters.org/thumbs/cover/640x480/" + text["data"]["print"]["images"].First()["filePath"].ToString(),
+                Link = "https://www.prusaprinters.org/prints/" + text["data"]["print"]["id"] + "-" + text["data"]["print"]["slug"]
             };
             mini.Sources.Add(new MiniSourceSite(mini, source, url));
 
