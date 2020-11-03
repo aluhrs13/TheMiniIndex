5b3fc57bbecd80a2f639b96c33130d3ca18af429
Andrew Luhrs <aluhrs@outlook.com>
2020-11-03 11:08:48 -0800

    Fixing AS merge

--- MiniIndex.Persistence/Configuration/SourceSiteEntityConfiguration.cs
+++ MiniIndex.Persistence/Configuration/SourceSiteEntityConfiguration.cs
@@ -20,6 +20,7 @@ namespace MiniIndex.Persistence.Configuration
                 .HasValue<Cults3dSource>("Cults3d")
                 .HasValue<Wargaming3dSource>("Wargaming3d")
                 .HasValue<DuncanShadowSource>("DuncanShadow")
+                .HasValue<PrusaPrintersSource>("AtelierStoria")
                 .HasValue<PrusaPrintersSource>("PrusaPrinters")
                 .HasValue<WebsiteSource>("Website");
 
