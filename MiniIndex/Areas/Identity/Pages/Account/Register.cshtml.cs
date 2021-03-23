7288f33ae82f38dae0721deb47f9d6b7a4b7a977
Andrew Luhrs <aluhrs@outlook.com>
2021-03-22 21:22:43 -0700

    Add ReCaptcha to registration

--- MiniIndex/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MiniIndex/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -13,6 +13,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using Google.Cloud.RecaptchaEnterprise.V1;
+using Microsoft.ApplicationInsights;
 
 namespace MiniIndex.Areas.Identity.Pages.Account
 {
@@ -23,17 +25,20 @@ namespace MiniIndex.Areas.Identity.Pages.Account
         private readonly UserManager<IdentityUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly TelemetryClient _telemetry;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            TelemetryClient telemetry)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _telemetry = telemetry;
         }
 
         [BindProperty]
@@ -74,8 +79,15 @@ namespace MiniIndex.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
+                if (!ReCaptchaPassed(Request.Form["g-recaptcha-response"]))
+                {
+                    ModelState.AddModelError(String.Empty, "Captcha failed, if you think this is a mistake please hit Feedback above and choose one of the options to reach out to us, we're getting spammed with fake registrations so we've had to add this extra security.");
+                    return Page();
+                }
+
                 IdentityUser user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
                 IdentityResult result = await _userManager.CreateAsync(user, Input.Password);
+
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
@@ -109,6 +121,75 @@ namespace MiniIndex.Areas.Identity.Pages.Account
 
             // If we got this far, something failed, redisplay form
             return Page();
+        }
+
+        public bool ReCaptchaPassed(string gRecaptchaResponse)
+        {
+            string SiteKey = "6LegSIoaAAAAADG_Bsv_EZPPFeBhOEmXTJg5Qx9u";
+            string Token = gRecaptchaResponse;
+            string AssessmentName = "RegistrationAssessment";
+            string ParentProject = "projects/tranquil-bison-277423";
+            string RecaptchaAction = "REGISTER";
+
+            RecaptchaEnterpriseServiceClientBuilder CredentialBuilder = new RecaptchaEnterpriseServiceClientBuilder();
+            CredentialBuilder.JsonCredentials = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS_JSON_VERSION");
+            RecaptchaEnterpriseServiceClient client = CredentialBuilder.Build();
+
+            CreateAssessmentRequest createAssessmentRequest = new CreateAssessmentRequest()
+            {
+                Assessment = new Assessment()
+                {
+                    Event = new Event()
+                    {
+                        SiteKey = SiteKey,
+                        Token = Token,
+                        ExpectedAction = RecaptchaAction
+                    },
+                    Name = AssessmentName,
+                },
+                Parent = ParentProject
+            };
+
+            Assessment response = client.CreateAssessment(createAssessmentRequest);
+
+            if (response.TokenProperties.Valid == false)
+            {
+                System.Console.WriteLine("The CreateAssessment() call failed " +
+                    "because the token was invalid for the following reason: " +
+                    response.TokenProperties.InvalidReason.ToString());
+
+                _telemetry.TrackEvent("ReCaptchaCompleted", new Dictionary<string, string> { { "RiskAnalysis", "-1" } });
+
+                return false;
+            }
+            else
+            {
+                if (response.Event.ExpectedAction == RecaptchaAction)
+                {
+                    System.Console.WriteLine("The reCAPTCHA score for this token is: " +
+                        response.RiskAnalysis.Score.ToString());
+
+                    _telemetry.TrackEvent("ReCaptchaCompleted", new Dictionary<string, string> { { "RiskAnalysis", response.RiskAnalysis.Score.ToString() } });
+                    if(response.RiskAnalysis.Score > .4)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    System.Console.WriteLine("The action attribute in your reCAPTCHA " +
+                        "tag does not match the action you are expecting to score");
+
+                    _telemetry.TrackEvent("ReCaptchaCompleted", new Dictionary<string, string> { { "RiskAnalysis", "-1" } });
+
+                    return false;
+                }
+
+            }
         }
     }
 }
