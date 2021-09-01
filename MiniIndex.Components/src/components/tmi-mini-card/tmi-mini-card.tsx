9e6dd38f44976bc7a24d1d8a9f65d2657ad63aba
Andy Luhrs <aluhrs@microsoft.com>
2021-08-31 20:22:05 -0700

    Setup Stencil for components

---
+++ MiniIndex.Components/src/components/tmi-mini-card/tmi-mini-card.tsx
@@ -0,0 +1,61 @@
+import { Component, Prop, h } from '@stencil/core';
+
+@Component({
+  tag: 'tmi-mini-card',
+  styleUrl: 'tmi-mini-card.css',
+  shadow: true,
+})
+export class TmiMiniCard {
+  /**
+   * Id of the Mini
+   */
+  @Prop() miniid: number;
+
+  /**
+   * Name of the Mini
+   */
+  @Prop() name: string;
+
+  /**
+   * Approval status of the Mini
+   */
+  @Prop() status: string;
+
+  /**
+   * Creator Details
+   */
+  @Prop() creatorname: string;
+
+  /**
+   * Creator Details
+   */
+  @Prop() creatorid: number;
+
+  /**
+   * Source site
+   */
+  @Prop() sourcesite: string;
+
+  render() {
+    console.log(this);
+    return (
+      <div class="card">
+        <div>
+          <a href={`https://beta.theminiindex.com/minis/${this.miniid}`}>
+            <img class="card-thumbnail" src={`https://miniindex.blob.core.windows.net/images/${this.miniid}.jpg`} />
+          </a>
+        </div>
+        <div class="card-text">
+          <img class="source-site-img" src={`https://miniindex.blob.core.windows.net/react-images/${this.sourcesite}.png`} />
+
+          <div class="mini-name">
+            <h1>{this.name}</h1>
+            <h2>
+              by <a href={`https://beta.theminiindex.com/creators/${this.creatorid}`}>{this.creatorname}</a>
+            </h2>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
