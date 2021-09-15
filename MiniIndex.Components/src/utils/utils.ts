0101e612f595c103c2291b053b22293a04054934
Andy Luhrs <aluhrs@microsoft.com>
2021-09-14 17:07:44 -0700

    Utils clean-up

--- MiniIndex.Components/src/utils/utils.ts
+++ MiniIndex.Components/src/utils/utils.ts
@@ -1,7 +1,3 @@
-export function format(first: string, middle: string, last: string): string {
-  return (first || '') + (middle ? ` ${middle}` : '') + (last ? ` ${last}` : '');
-}
-
 export function fixCDN(thumbnail: string) {
   if (thumbnail.includes('miniindex.blob.core.windows.net')) {
     return thumbnail.replace('miniindex.blob.core.windows.net', 'miniindexblobakamai.azureedge.net');
