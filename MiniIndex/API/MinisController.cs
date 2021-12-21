61207d73c4d78eae4771e54404eafd0600cd4a2d
Andy Luhrs <aluhrs@outlook.com>
2021-12-21 13:39:28 -0800

    Move loading of related Minis to be async

--- MiniIndex/API/MinisController.cs
+++ MiniIndex/API/MinisController.cs
@@ -144,6 +144,7 @@ namespace MiniIndex.API
             }
         }
 
+        //TODO: Perf on this isn't good.
         [HttpGet("{id}/Related")]
         public async Task<IActionResult> Related(int id)
         {
@@ -225,7 +226,9 @@ namespace MiniIndex.API
             RelatedMinis = RelatedMinis.Distinct()
                                 .Where(m => m.Status == Status.Approved)
                                 .Where(m => m.ID != mini.ID)
-                                .OrderByDescending(m => m.ApprovedTime).ToList();
+                                .OrderByDescending(m => m.ApprovedTime)
+                                .Take(6)
+                                .ToList();
             
             return Ok(RelatedMinis);
         }
@@ -264,7 +267,7 @@ namespace MiniIndex.API
         public async Task<IActionResult> FixThumbnail(int id)
         {
             Mini currentMini = await _context.Set<Mini>().FirstOrDefaultAsync(m => m.ID == id);
-            Mini mini = await _mediator.Send(new MiniSubmissionRequest(new Uri(currentMini.Link), user, true));
+            Mini mini = await _mediator.Send(new MiniSubmissionRequest(new Uri(currentMini.Link), null, true));
 
             if (mini != null)
             {
